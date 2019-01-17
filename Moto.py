145af05121cb3fdcc363d577e80e406acf43a255
Toxiko8 <arnaud.clarat@live.fr>
2019-01-17 20:07:34 +0100

    Passé le stockage en DB SQLite

--- Moto.py
+++ Moto.py
@@ -1,4 +1,6 @@
 import string
+import sqlite3
+from sqlite3 import Error
 
 def strToList(string): 
 	li = []
@@ -22,45 +24,44 @@ def numOnly (string) :
 	return retour
 
 def newMoto():
-	moto=""
-	moto += input("\nMarque? ") + ";"
-	moto += input("Modèle? ") + ";"
-	moto += input("Prix? ") + ";"
-	moto += input("Note personnelle? /100 ") + "\n"
-	return moto
+	marque = input("\nMarque? ")
+	modele = input("Modèle? ")
+	prix = input("Prix? ")
+	notePerso= input("Note personnelle? /100 ")
+	return marque, modele, prix, notePerso
+
+def remplissage():
+		# Opération sur la DB
+		cur.execute("CREATE TABLE IF NOT EXISTS motos (id INTEGER PRIMARY KEY, marque TEXT, modele TEXT, prix INTEGER, notePerso INTEGER)")
+		cur.execute("INSERT INTO motos (marque, modele, prix, notePerso) VALUES(?,?,?,?)",(newMoto()))
+		db.commit()
 	
 def marqueMoto(ligne):
 	moto = {}
 	moto["Marque"], moto["Modèle"], moto["Prix"], moto["Note personnelle"] = ligne.split(";")
 	return moto
 
-def selectMoto():
-	menu = "a"
-	i = 0
-	print("\nListe Moto :")
-	with open("motoDB.csv", "r") as dataBase :
-		for moto in dataBase :
-			dicoMoto = marqueMoto(moto)
-			print(i, ":", dicoMoto["Marque"], dicoMoto["Modèle"])
-			i += 1
-		menu2 = input("Quelle moto voulez-vous choisir? ")
-		try :
-			test = int(menu2) or int(menu2) < 9
-		except :
-			menu2 = input("Veuillez saisir un numéro correct : ")
-		print("\nListe Info :")
-		dataBase.seek(0,int(menu2))
-		selectMoto = ((dataBase.readline()[:-1]).split(";"))
-		print(selectMoto)
-		for i in selectMoto :
-			print(i)
-	return selectMoto
-
 def modMoto():
 	a = 0
 
-def viewMoto():
-	a = 0
+def viewMoto(cur) :
+	print("Liste des motos :\n")
+	cur.execute("SELECT id, marque, modele FROM motos")
+	for moto in cur :
+		print(moto)
+	menu1 = input("Quelle moto voulez-vous choisir? ")
+	try :
+		test = int(menu1) or int(menu1) < 9
+	except :
+		menu1 = input("Veuillez saisir un numéro correct : ")
+	print("\nListe info :")
+	cur.execute("SELECT * FROM motos WHERE id = ?",(menu1))
+	for donnee in cur :
+		print(donnee)
+	menu2 = input("Voulez-vous la modifier? ")
+	if menu2 == "oui" :
+		a = 0
+
 def delMoto():
 	a = 0
 def bestMoto():
@@ -72,6 +73,12 @@ menu = "x"
 
 #Programme
 print("\nBienvenue dans ce comparateur de moto!")
+try:
+    db = sqlite3.connect('data/moto.db')
+    cur = db.cursor()
+except Error as e:
+    print(e)
+
 while menu != "Q" :
 	print("\nMenu :")
 	print("1 = Ajouter une moto")
@@ -82,15 +89,17 @@ while menu != "Q" :
 	print("Q = Quitter le programme")
 	menu = input("Que souhaitez-vous faire? ")
 
+
 	if menu == "1" : 
-		with open("motoDB.csv", "a") as dataBase :
-			dataBase.write(newMoto())
+		remplissage()
 	if menu == "2" :
-		selectMoto()
-		input("Appuyez sur entrée pour continuer")
+		viewMoto(cur)
 	if menu == "3" :
 		a = 3
 	if menu == "4" :
 		a = 4
 	if menu == "5" :
 		a = 5
+
+db.commit()
+db.close()
